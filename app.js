b4614fe1e28898bddccb582b88abdcff8a8578bd
魏毅博 <yibo.wei@17zuoye.com>
2020-08-04 10:40:51 +0800

    logger static moment

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const path = require('path');
 const Koa = require('koa');
-const nunjucks = require('koa-nunjucks-2');
-const bodyParser = require('koa-bodyparser');
+const path = require('path');
+const nunjucks = require('koa-nunjucks-2'); // 模板引擎
+const bodyParser = require('koa-bodyparser'); // 处理 post 请求数据
 const router = require('./router.js');
+const server = require('koa-static'); // 静态文件
+const logger = require('koa-logger'); // log 日志
+const moment = require('moment');
 
-// 若项目中使用了不下一种模板引擎 如 swig ejs 等等 ，可引入 co-view 组织模板引擎解决
+// 若项目中使用了不下一种模板引擎 如 swig ejs nunjucks 等等 ，可引入 co-view 组织模板引擎解决
 
 const app = new Koa();
 
@@ -18,6 +21,10 @@ app.use(nunjucks({ // 为 app.context 提供一个 render 方法
 
 app.use(bodyParser());
 router(app);
+app.use(server('.'));
+app.use(logger((str) => {
+    console.log(`${moment().format('YYYY-MM-DD hh:mm:ss')}${str}`);
+}));
 
 app.listen(9978, () => {
     console.log(`Local serve is running at http://localhost:9978/`);
