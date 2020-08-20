02504bd19193b890bb34a00df671dff201be26c9
魏毅博 <yibo.wei@17zuoye.com>
2020-08-20 18:43:03 +0800

    优化

--- gitOp.sh
+++ gitOp.sh
@@ -1,57 +1,65 @@
 #!/bin/sh
 
-#脚本执行需要2个参数
+#脚本运行命令 sh gitOp.sh new_branch
 
-if [ $# -eq 1 ]; then
+#配置默认基础分支
 
-  echo "开始执行git脚本..."
+default_branch="master"
 
-  echo "你的新建分支名称：$1，你要基于哪个分支：$2，"
+if [ ! $1 ]; then
 
-else
-
-  echo "脚本执行需要1个参数：你的新建分支名称，你要基于哪个分支（若不配默认为master）"
+  echo "脚本执行需要至少1个参数：你的新建分支名称"
 
 exit -1
 
-fi
-
-#你所要创建的分支
-
-new_branch_name=$1
-
-#你所要基于的分支
-
-if [$2]; then
-
-  base_branch_name=$2
-
 else
 
-  base_branch_name="master"
+  new_branch_name=$1
+  if [ ! $2 ]; then
+
+    base_branch_name="$default_branch"
+
+  else
+
+    base_branch_name=$2
+
+  fi
+
+  echo "开始基于 $base_branch_name 分支创建 $new_branch_name 分支..."
 
 fi
+
 
 #切换至 base_branch_name 分支
 
 git checkout "$base_branch_name"
 
+#pull 操作
+
+git pull
+
+if [ $? -ne 0 ]; then
+  echo "git pull 错误"
+  exit -1
+fi
+
 #新建 new_branch_name 分支
 
 git branch "$new_branch_name"
+
+#切换至 new_branch_name 分支
+
+git checkout "$new_branch_name"
 
 #git push,创建远程分支
 
 git push origin "$new_branch_name":"$new_branch_name"
 
 if [ $? -ne 0 ]; then
-
   echo "git push 错误"
-
   exit -1
-
 fi
 
 #查看分支建立情况
 
-echo "you can open IntelliJ IDEA to write Java code..."
+echo "$new_branch_name 分支已创建成功，请开始你的表演..."
